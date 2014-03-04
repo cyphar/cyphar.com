6bd0c1c56cb5c933fd63470ce8744b0f8ea95b8c
cyphar <cyphar@cyphar.com>
2014-03-04 11:46:08 +1100

    db: added database api

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+-- Applies the cyphar.com schemas
+-- Run clean.sql *before* running this.
+
+CREATE TABLE IF NOT EXISTS tbl_contacts (
+	priority INTEGER NOT NULL,
+	contact TEXT NOT NULL,
+	url TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tbl_projects (
+	priority INTEGER NOT NULL,
+	project TEXT NOT NULL,
+	language TEXT,
+	url TEXT,
+	description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tbl_acks (
+	priority INTEGER NOT NULL,
+	ack TEXT NOT NULL,
+	vuln TEXT,
+	url TEXT,
+	description TEXT
+);
