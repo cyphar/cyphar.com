87b775fcf216c43099dedaa2172ef7a308f77fe6
cyphar <cyphar@cyphar.com>
2014-03-04 17:05:13 +1100

    db: updated api to work better with flask

--- db/api.py
+++ db/api.py
@@ -2,21 +2,14 @@
 
 import sqlite3
 
-TBL_CONTACTS = ""
-TBL_PROJECTS = ""
-TBL_ACKS = ""
+TBL_CONTACTS = "tbl_contacts"
+TBL_PROJECTS = "tbl_projects"
+TBL_KUDOS    = "tbl_kudos"
 
-_conn = None
-
-def init(fname):
-	global _conn
+def conn(fname):
 	_conn = sqlite3.connect(fname)
 	_conn.row_factory = sqlite3.Row
-
-def fini():
-	global _conn
-	if _conn:
-		_conn.close()
+	return _conn
 
 
 class Contact:
@@ -26,19 +19,19 @@ class Contact:
 		self.url = url or None
 
 	@classsmethod
-	def findall(cls):
-		cur = _conn.execute("SELECT priority, contact, url FROM ? ORDER BY priority DESC", (TBL_CONTACTS, ))
+	def findall(cls, conn):
+		cur = conn.execute("SELECT priority, contact, url FROM ? ORDER BY priority DESC", (TBL_CONTACTS, ))
 		return [cls(**item) for item in cur.fetchall()]
 
 	@classmethod
-	def create(cls, priority, contact, url=None):
+	def create(cls, conn, priority, contact, url=None):
 		self = cls(priority, contact, url)
-		self.save()
+		self.save(conn)
 		return self
 
-	def save(self):
-		_conn.execute("INSERT INTO ? (priority, contact, url) VALUES (?, ?, ?)", (TBL_CONTACTS, self.priority, self.contact, self.url))
-		_conn.commit()
+	def save(self, conn):
+		conn.execute("INSERT INTO ? (priority, contact, url) VALUES (?, ?, ?)", (TBL_CONTACTS, self.priority, self.contact, self.url))
+		conn.commit()
 
 
 class Project:
@@ -50,23 +43,23 @@ class Project:
 		self.description = description or None
 
 	@classsmethod
-	def findall(cls):
-		cur = _conn.execute("SELECT priority, project, language, url, description FROM ? ORDER BY priority DESC", (TBL_PROJECTS, ))
+	def findall(cls, conn):
+		cur = conn.execute("SELECT priority, project, language, url, description FROM ? ORDER BY priority DESC", (TBL_PROJECTS, ))
 		return [cls(**item) for item in cur.fetchall()]
 
 	@classmethod
-	def create(cls, priority, project, language=None, url=None, description=None):
+	def create(cls, conn, priority, project, language=None, url=None, description=None):
 		self = cls(priority, project, language, url, description)
-		self.save()
+		self.save(conn)
 		return self
 
 	def save(self):
-		_conn.execute("INSERT INTO ? (priority, project, language, url, description) VALUES (?, ?, ?, ?, ?)",
+		conn.execute("INSERT INTO ? (priority, project, language, url, description) VALUES (?, ?, ?, ?, ?)",
 				(TBL_CONTACTS, self.priority, self.project, self.language, self.url, self.description))
-		_conn.commit()
+		conn.commit()
 
 
-class Ack:
+class Kudos:
 	def __init__(self, priority, ack, vuln=None, url=None, description=None):
 		self.priority = priority
 		self.ack = ack
@@ -75,16 +68,16 @@ class Ack:
 		self.description = description or None
 
 	@classsmethod
-	def findall(cls):
-		cur = _conn.execute("SELECT priority, ack, vuln, url, description FROM ? ORDER BY priority DESC", (TBL_ACKS, ))
+	def findall(cls, conn):
+		cur = conn.execute("SELECT priority, ack, vuln, url, description FROM ? ORDER BY priority DESC", (TBL_KUDOS, ))
 		return [cls(**item) for item in cur.fetchall()]
 
 	@classmethod
-	def create(cls, priority, project, language=None, url=None, description=None):
+	def create(cls, conn, priority, project, language=None, url=None, description=None):
 		self = cls(priority, project, language, url, description)
-		self.save()
+		self.save(conn)
 		return self
 
 	def save(self):
-		_conn.execute("INSERT INTO ? (priority, ack, vuln, url, description) VALUES (?, ?, ?, ?, ?)", (TBL_CONTACTS, self.priority, self.ack, self.vuln, self.url, self.description))
-		_conn.commit()
+		conn.execute("INSERT INTO ? (priority, ack, vuln, url, description) VALUES (?, ?, ?, ?, ?)", (TBL_CONTACTS, self.priority, self.ack, self.vuln, self.url, self.description))
+		conn.commit()
