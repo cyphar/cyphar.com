23c2395d16c1d45d4f8311d3a07ff922841f0bd3
Aleksa Sarai <cyphar@cyphar.com>
2014-03-22 18:40:40 +1100

    db: data: change to /src links to github

--- db/data.sql
+++ db/data.sql
@@ -16,15 +16,15 @@ INSERT INTO tbl_contacts (priority, contact, url) VALUES (-10, "LinkedIn", "http
 
 INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (120, "dotRush", "Java", "https://play.google.com/store/apps/details?id=com.jtdev.dotrush", "Very addictive android game. Eat the smaller dots to grow and eat bigger dots. Just don't get dotrush'd.");
 INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (110, "ninjabot", "Python", "https://github.com/ackwell/ninjabot", "Modular IRC bot written in Python. Supports dynamic reloading of bot modules and core.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (100, "rawline", "C", "https://github.com/cyphar/rawline", "The small and self-contained line-editing library. Written in less than 1000 lines of ANSI C and can easily be included in any project.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (80, "perfectgift", "Python + SQL", "https://github.com/cyphar/perfectgift", "A tornado webapp, written by Group 4 at the NCSS Summer School 2014 all-nighter. Get the perfect gift for your friends, with this innovative wishlist app.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (70, "epyc", "Python", "https://github.com/cyphar/epyc", "A python templating language (written for <a href='https://github.com/cyphar/perfectgift'>perfectgift</a>).");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (60, "synge", "C", "https://github.com/cyphar/synge", "Very powerful scientific calculation engine. Supports variables, short-cutting conditions and recursive expressions.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (50, "copper", "C", "https://github.com/cyphar/copper", "Cu, a C userland.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (40, "ttu", "C", "https://github.com/cyphar/ttu", "Small program which silently converts TCP sockets to Unix sockets for any standard *nix executable.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (30, "tic-tac-toe", "C", "https://github.com/cyphar/tic-tac-toe", "The classic game, boasting a heuristic AI. Supports zero, one or two players.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (20, "ascii-snake", "C", "https://github.com/cyphar/ascii-snake", "Remake of the Nokia snake game for *nix consoles, with several compile-time configuration options.");
-INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (10, "moss", "C", "https://github.com/cyphar/moss", "A simple command-line file viewer. Supports regex searching, multiple files, switching to <code>$EDITOR</code> and file reloading.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (100, "rawline", "C", "/src/rawline", "The small and self-contained line-editing library. Written in less than 1000 lines of ANSI C and can easily be included in any project.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (80, "perfectgift", "Python + SQL", "/src/perfectgift", "A tornado webapp, written by Group 4 at the NCSS Summer School 2014 all-nighter. Get the perfect gift for your friends, with this innovative wishlist app.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (70, "epyc", "Python", "/src/epyc", "A python templating language (written for <a href='/src/perfectgift'>perfectgift</a>).");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (60, "synge", "C", "/src/synge", "Very powerful scientific calculation engine. Supports variables, short-cutting conditions and recursive expressions.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (50, "copper", "C", "/src/copper", "Cu, a C userland.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (40, "ttu", "C", "/src/ttu", "Small program which silently converts TCP sockets to Unix sockets for any standard *nix executable.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (30, "tic-tac-toe", "C", "/src/tic-tac-toe", "The classic game, boasting a heuristic AI. Supports zero, one or two players.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (20, "ascii-snake", "C", "/src/ascii-snake", "Remake of the Nokia snake game for *nix consoles, with several compile-time configuration options.");
+INSERT INTO tbl_projects (priority, project, language, url, description) VALUES (10, "moss", "C", "/src/moss", "A simple command-line file viewer. Supports regex searching, multiple files, switching to <code>$EDITOR</code> and file reloading.");
 
 -----------
 -- KUDOS --
