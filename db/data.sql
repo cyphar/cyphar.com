c865a6a1f3b09ac6a4593ec2006092cb5d0d6a38
cyphar <cyphar@cyphar.com>
2014-04-02 19:58:56 +1100

    db: data: update ms link

--- db/data.sql
+++ db/data.sql
@@ -51,7 +51,7 @@ INSERT INTO tbl_projects (priority, project, language, url, description) VALUES 
 -- KUDOS --
 -----------
 
-INSERT INTO tbl_kudos (priority, ack, vuln, url, description) VALUES (20, "Microsoft (Online Services)", "Coldfusion Exploit (Root Access)", "http://technet.microsoft.com/en-us/security/cc308589", "Due to an outdated version of Coldfusion installed on a Microsoft MSN server, I was able to bypass the administrative logon and gain administrative access. This would allow me to schedule tasks to run as a privelliged user, such as reverse shells, create users, etc.");
+INSERT INTO tbl_kudos (priority, ack, vuln, url, description) VALUES (20, "Microsoft (Online Services)", "Coldfusion Exploit (Root Access)", "http://technet.microsoft.com/en-us/security/cc308575#0214", "Due to an outdated version of Coldfusion installed on a Microsoft MSN server, I was able to bypass the administrative logon and gain administrative access. This would allow me to schedule tasks to run as a privelliged user, such as reverse shells, create users, etc.");
 INSERT INTO tbl_kudos (priority, ack, vuln, url, description) VALUES (10, "Grok Learning", "Sandbox Bypass", "https://groklearning.com/security", "Due to a misconfiguration in the testing machine's firewall, the sandbox could access the internet. This allowed for the disclosure of test data (as well as possible exploitation vectors).");
 
 ------------------
