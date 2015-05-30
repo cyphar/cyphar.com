9e5db81bb1155b7c510ed52dfa5fb3c485c86e05
Aleksa Sarai <cyphar@cyphar.com>
2015-05-30 15:27:24 +1000

    *: bump year

--- cyphar.py
+++ cyphar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # cyphar.com: my personal site's flask app
-# Copyright (c) 2014 Cyphar
+# Copyright (C) 2014, 2015 Cyphar
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
