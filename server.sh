4f83a57f433cca927d4f5554566037d5eeb73ed2
Aleksa Sarai <cyphar@cyphar.com>
2019-01-20 22:02:12 +1100

    deploy: switch to gunicorn
    Rather than using the "development" Flask server, use gunicorn which is
    generally preferred to Flask since it can use more than one process at a
    time.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- server.sh
+++ server.sh
@@ -1,42 +1,21 @@
+#!/bin/sh
 # cyphar.com: my personal site's flask app
 # Copyright (C) 2014, 2015, 2016, 2018 Aleksa Sarai
-
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-FROM opensuse/leap:latest
-MAINTAINER "Aleksa Sarai <cyphar@cyphar.com>"
+set -Eeuxo pipefail
 
-# Make sure the repos and packages are up to date
-RUN zypper up --no-confirm && \
-    zypper in --no-confirm --no-recommends python3 python3-pip git && \
-    zypper clean --all
-
-# Set up server user and directory.
-RUN useradd -s /bin/false -d /srv/www -- drone
-
-# Install Python requirements.
-COPY requirements.txt /requirements.txt
-RUN pip3 --no-cache-dir install -r /requirements.txt
-
-# Set up cyphar.com and port config.
-ARG PORT=5000
-USER drone
-EXPOSE $PORT
-WORKDIR /srv/www
-ENTRYPOINT ["./server.sh"]
-CMD []
-
-# Copy over the cyphar.com app source.
-# Do this last to preserve the cache.
-COPY . /srv/www
+PORT="${PORT:-5000}"
+exec gunicorn --bind "0.0.0.0:$PORT" --access-logfile - cyphar:app
