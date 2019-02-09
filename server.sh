db51d9dc480925eace48a0beb9cab83cf9deb6ef
Aleksa Sarai <cyphar@cyphar.com>
2019-02-09 11:24:44 +1100

    *: update copyright years
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # cyphar.com: my personal site's flask app
-# Copyright (C) 2014, 2015, 2016, 2018 Aleksa Sarai
+# Copyright (C) 2014-2019 Aleksa Sarai
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
